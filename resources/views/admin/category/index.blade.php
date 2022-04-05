29131b278a90731bbaecf97622cd44d8e3aba47b
Faiz Ghazali <kisame1990@gmail.com>
2022-04-05 15:23:48 -0700

    5.4 Progress

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -35,17 +35,17 @@
                                 </tr>
                             </thead>
                             <tbody>
-                                        @php($i= 1)
+                                        <!-- @php($i= 1) -->
                                         @foreach ($categories as $category)
                                     <tr>
-                                    <th scope="row">{{$i++}}</th>
+                                    <th scope="row">{{ $categories->firstItem()+$loop->index}}</th>
                                     <td>{{$category->category_name}}</td>
-                                    <td>{{$category->user_id}}</td>
+                                    <td>{{$category->name}}</td>
                                     <td>
                                     @if($category->created_at == NULL)
                                     <span class="text-danger"> No Date Set</span>
                                       @else  
-                                    {{$category->created_at->diffforHumans()}}
+                                    {{ Carbon\Carbon::parse($category->created_at)->diffforHumans()}}
                                     @endif
                                     </td>
                                     </tr>
@@ -53,6 +53,7 @@
                                 
                             </tbody>
                         </table>
+                        {{ $categories->links() }}
 
                     </div>
                 </div>
