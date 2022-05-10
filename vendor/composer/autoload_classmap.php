07bb7472ad1887f75b85acc10f1d47082cadd26e
Faiz Ghazali <kisame1990@gmail.com>
2022-05-10 16:50:14 +0800

    added Brand sub menu

--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -29,6 +29,7 @@ return array(
     'App\\Http\\Middleware\\VerifyCsrfToken' => $baseDir . '/app/Http/Middleware/VerifyCsrfToken.php',
     'App\\Models\\Category' => $baseDir . '/app/Models/Category.php',
     'App\\Models\\User' => $baseDir . '/app/Models/User.php',
+    'App\\Models\\brand' => $baseDir . '/app/Models/brand.php',
     'App\\Providers\\AppServiceProvider' => $baseDir . '/app/Providers/AppServiceProvider.php',
     'App\\Providers\\AuthServiceProvider' => $baseDir . '/app/Providers/AuthServiceProvider.php',
     'App\\Providers\\BroadcastServiceProvider' => $baseDir . '/app/Providers/BroadcastServiceProvider.php',
